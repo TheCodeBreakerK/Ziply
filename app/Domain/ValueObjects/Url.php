2e69eb01cb62c93c10730a1cfc3bbd7d19592067
TheCodeBreakerK <kelvin20mso@gmail.com>
2025-05-09 20:17:45 -0300

    refactor(app/Domain/ValueObjects/Url.php): add news validations

--- app/Domain/ValueObjects/Url.php
+++ app/Domain/ValueObjects/Url.php
@@ -6,6 +6,7 @@ use http\Exception\InvalidArgumentException;
 
 class Url
 {
+    private const int MAX_URL_LENGTH = 2000;
     private string $value;
 
     public function __construct(string $url)
@@ -17,7 +18,15 @@ class Url
     public function validate(string $url): void
     {
         if (!filter_var($url, FILTER_VALIDATE_URL)) {
-            throw new InvalidArgumentException("Invalid URL");
+            throw new InvalidArgumentException('Invalid URL format');
+        }
+
+        if (!preg_match('/^https?:\/\//i', $url)) {
+            throw new InvalidArgumentException('Only HTTP and HTTPS protocols are allowed');
+        }
+
+        if (strlen($url) > self::MAX_URL_LENGTH) {
+            throw new InvalidArgumentException('URL exceeds maximum length of ' . self::MAX_URL_LENGTH . ' characters');
         }
     }
 
